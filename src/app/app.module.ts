d670362eb1881800c29d096b1f87bc923c680f99
uzidreamcode <buatoslinux@gmail.com>
2025-01-16 10:18:41 +0700

    add new component with template, styles, and tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './component.component.html',
+  styleUrls: ['./component.component.css']
+})
+export class ComponentComponent {
+  name = 'Angular';
+
+}
